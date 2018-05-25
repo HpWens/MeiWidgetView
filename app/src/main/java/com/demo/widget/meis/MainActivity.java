218aceb05415a0d2431918d31facafad0e25f9d0
wenshi <373872690@qq.com>
2018-05-25 11:30:02 +0800

    v0.0.8 demo 新增转场进入小视频列表

--- app/src/main/java/com/demo/widget/meis/MainActivity.java
+++ app/src/main/java/com/demo/widget/meis/MainActivity.java
@@ -1,8 +1,11 @@
 package com.demo.widget.meis;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
